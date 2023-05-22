f16915b9f0ab3a5a42098a74c1d928fba60f7817
Denis karpovskii <geext29@gmail.com>
2023-05-22 11:00:12 +0200

    Fix e2e tests startup

--- test/gql-plugin.js
+++ test/gql-plugin.js
@@ -5,7 +5,7 @@ module.exports.name = 'nestjs-graphql-transformer'
 // you should change the version number anytime you change the configuration below - otherwise, jest will not detect changes
 module.exports.version = 1
 
-module.exports.factory = (cs: { program: unknown }) => {
+module.exports.factory = (cs) => {
   return transformer.before(
     {
       // @nestjs/graphql/plugin options (can be empty)
